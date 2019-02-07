922d94ca2b6b11f50b94216f1556b967b4be85cd
Nicole Ridout <nicole.ridout@gmail.com>
2019-02-07 17:20:33 +0000

    completed refactoring by replacing for loop with forEach

--- findwaldo.js
+++ findwaldo.js
@@ -1,14 +1,20 @@
+//  function that finds "Waldo" in an array of strings and returns Waldo's index number
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  //  for each element in the array (where element is the value & index is the position in the array)
+  arr.forEach(function(element, index) {
+    //  if the array value is Waldo
+    if (element === "Waldo") {
+      //  execute found
+      found(index);   // execute callback
     }
-  }
+  });
 }
 
-function actionWhenFound(i) {
-  var index = i;
+//prints where waldo is in the array (index position)
+function actionWhenFound(index) {
   console.log("Found Waldo at index " + index);
 }
-
+//passing the array through function find waldo & executing action when found
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+//actionWhenFound becomes "found" when passed through the findWaldo function
